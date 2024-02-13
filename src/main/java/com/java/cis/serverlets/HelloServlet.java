6df1ee814aff9d45984c63a704b77817ae4cc351
mandha03 <146457500+mandha03@users.noreply.github.com>
2024-02-13 17:33:47 -0600

    Servlets.

--- src/main/java/com/java/cis/serverlets/HelloServlet.java
+++ src/main/java/com/java/cis/serverlets/HelloServlet.java
@@ -1,4 +1,4 @@
-package com.java.cis;
+package com.java.cis.serverlets;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -14,6 +14,9 @@ public class HelloServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         PrintWriter printOut = response.getWriter();
         printOut.write("Hello, This is the first servlet 3 annotation example");
+
+
+
     }
 
 }
